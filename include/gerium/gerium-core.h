83daeb987c222d3347df7526eb52726e1153c2ca
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-06-26 09:03:50 +0300

    Add function gerium_state_to_string

--- include/gerium/gerium-core.h
+++ include/gerium/gerium-core.h
@@ -13,11 +13,25 @@
 
 GERIUM_BEGIN
 
+typedef enum
+{
+   GERIUM_STATE_SUCCESS               = 0,
+   GERIUM_STATE_UNKNOWN_ERROR         = 1,
+   GERIUM_STATE_OUT_OF_MEMORY         = 2,
+   GERIUM_STATE_NOT_IMPLEMENTED       = 3,
+   GERIUM_STATE_FEATURE_NOT_SUPPORTED = 4,
+   GERIUM_STATE_INVALID_ARGUMENT      = 5,
+   GERIUM_STATE_MAX_ENUM              = 0x7FFFFFFF
+} gerium_state_t;
+
 gerium_public gerium_uint32_t
 gerium_version(void);
 
 gerium_public gerium_utf8_t
 gerium_version_string(void);
+
+gerium_public gerium_utf8_t
+gerium_state_to_string(gerium_state_t state);
 
 GERIUM_END
 
--- sources/Gerium.cpp
+++ sources/Gerium.cpp
@@ -7,3 +7,28 @@ gerium_uint32_t gerium_version(void) {
 gerium_utf8_t gerium_version_string(void) {
     return GERIUM_VERSION_STRING;
 }
+
+gerium_utf8_t gerium_state_to_string(gerium_state_t state) {
+    switch (state) {
+        case GERIUM_STATE_SUCCESS:
+            return "no error has occurred";
+
+        case GERIUM_STATE_UNKNOWN_ERROR:
+            return "out of memory";
+
+        case GERIUM_STATE_OUT_OF_MEMORY:
+            return "unknown error";
+
+        case GERIUM_STATE_NOT_IMPLEMENTED:
+            return "not implemented";
+
+        case GERIUM_STATE_FEATURE_NOT_SUPPORTED:
+            return "feature not supported";
+
+        case GERIUM_STATE_INVALID_ARGUMENT:
+            return "invalid argument passed";
+
+        default:
+            return "<unknown state>";
+    }
+}
