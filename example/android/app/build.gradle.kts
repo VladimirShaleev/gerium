4250309d0716221e907d8d7ba71c375676825e76
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-07-09 17:49:15 +0300

    Add example for Android

---
+++ example/android/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.github.vladimirshaleev.gerium"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.github.vladimirshaleev.gerium"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        @Suppress("UnstableApiUsage") externalNativeBuild {
+            cmake {
+                arguments += "-DGERIUM_BUILD_TESTS=OFF"
+                arguments += "-DANDROID_TOOLCHAIN=clang"
+                arguments += "-DANDROID_STL=c++_static"
+                arguments += "-DCMAKE_TOOLCHAIN_FILE="
+                cppFlags += "-std=c++20"
+            }
+        }
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("../../../CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+}
