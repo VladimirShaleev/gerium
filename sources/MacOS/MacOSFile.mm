97503bb4d6d72c217d7d9077d5e40c88b948a566
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-07-25 19:25:59 +0300

    Implement File::getCacheDir and File::getAppDir for MacOS

--- sources/MacOS/MacOSFile.mm
+++ sources/MacOS/MacOSFile.mm
@@ -16,6 +16,20 @@ MacOSFile::MacOSFile(gerium_utf8_t path, bool readOnly) : File(readOnly) {
 MacOSFile::~MacOSFile() {
 }
 
+std::string MacOSFile::getPath(NSSearchPathDirectory directory) noexcept {
+    std::string dir;
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(directory, NSUserDomainMask, YES);
+    if ([paths count] > 0) {
+        NSString* firstPath = [paths objectAtIndex:0];
+        NSString* path = [firstPath stringByAppendingPathComponent:[[NSBundle mainBundle] bundleIdentifier]];
+        if ([path compare:firstPath] == NSOrderedSame) {
+            path = [firstPath stringByAppendingPathComponent:[[NSProcessInfo processInfo] processName]];
+        }
+        dir = [path UTF8String];
+    }
+    return dir;
+}
+
 gerium_uint64_t MacOSFile::onGetSize() noexcept {
 }
 
@@ -34,9 +48,13 @@ gerium_data_t MacOSFile::onMap() noexcept {
 } // namespace macos
 
 gerium_utf8_t File::getCacheDir() noexcept {
+    static auto dir = macos::MacOSFile::getPath(NSCachesDirectory);
+    return dir.c_str();
 }
 
 gerium_utf8_t File::getAppDir() noexcept {
+    static auto dir = macos::MacOSFile::getPath(NSApplicationSupportDirectory);
+    return dir.c_str();
 }
 
 bool File::existsFile(gerium_utf8_t path) noexcept {
