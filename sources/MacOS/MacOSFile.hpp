cd6f49a5ea8decf82ac62e1985f0b5bd933546c1
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-07-27 06:50:56 +0300

    Update MacOSFile

--- sources/MacOS/MacOSFile.hpp
+++ sources/MacOS/MacOSFile.hpp
@@ -1,20 +1,18 @@
 #ifndef GERIUM_MAC_OS_MAC_OS_FILE_HPP
 #define GERIUM_MAC_OS_MAC_OS_FILE_HPP
 
-#include "../File.hpp"
+#include "../Unix/UnixFile.hpp"
 
 #import <AppKit/AppKit.h>
 #import <Foundation/Foundation.h>
 
 namespace gerium::macos {
 
-class MacOSFile final : public File {
+class MacOSFile final : unix::UnixFile {
 public:
     explicit MacOSFile(gerium_uint64_t size);
     MacOSFile(gerium_utf8_t path, gerium_uint64_t size);
     MacOSFile(gerium_utf8_t path, bool readOnly);
-
-    ~MacOSFile() override;
 
     static std::string getPath(NSSearchPathDirectory directory) noexcept;
     static bool exists(gerium_utf8_t path, bool isDir) noexcept;
@@ -22,15 +20,6 @@ public:
 private:
     static std::string getTempFile();
     static NSString* appendBundleId(NSString* path);
-    
-    gerium_uint64_t onGetSize() noexcept override;
-    void onSeek(gerium_uint64_t offset, gerium_file_seek_t seek) noexcept override;
-    void onWrite(gerium_cdata_t data, gerium_uint32_t size) override;
-    gerium_uint32_t onRead(gerium_data_t data, gerium_uint32_t size) noexcept override;
-    gerium_data_t onMap() noexcept override;
-    
-    int _file;
-    gerium_data_t _data;
 };
 
 } // namespace gerium::macos
--- sources/MacOS/MacOSFile.mm
+++ sources/MacOS/MacOSFile.mm
@@ -1,10 +1,5 @@
 #include "MacOSFile.hpp"
 
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
 namespace gerium {
 
 namespace macos {
@@ -12,48 +7,10 @@ namespace macos {
 MacOSFile::MacOSFile(gerium_uint64_t size) : MacOSFile(getTempFile().c_str(), size) {
 }
 
-MacOSFile::MacOSFile(gerium_utf8_t path, gerium_uint64_t size) : File(false), _file(-1), _data(nullptr) {
-    
-    const auto dirs = std::filesystem::path(path).parent_path().string();
-    ::mkdir(dirs.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-    
-    _file = ::open(path, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
-    if (_file < 0) {
-        error(GERIUM_RESULT_ERROR_UNKNOWN); // TODO: add err
-    }
-
-    if (size) {
-        fstore_t store = { F_ALLOCATECONTIG, F_PEOFPOSMODE, 0, (off_t) size };
-        if (fcntl(_file, F_PREALLOCATE, &store) == -1) {
-            error(GERIUM_RESULT_ERROR_UNKNOWN); // TODO: add err
-        }
-        if (ftruncate(_file, (off_t) size) != 0) {
-            error(GERIUM_RESULT_ERROR_UNKNOWN); // TODO: add err
-        }
-    }
+MacOSFile::MacOSFile(gerium_utf8_t path, gerium_uint64_t size) : unix::UnixFile(path, size) {
 }
 
-MacOSFile::MacOSFile(gerium_utf8_t path, bool readOnly) : File(readOnly), _file(-1), _data(nullptr) {
-    
-    const auto dirs = std::filesystem::path(path).parent_path().string();
-    ::mkdir(dirs.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-    
-    _file = ::open(path, (readOnly ? O_RDONLY : O_RDWR) | O_CREAT, S_IRUSR | (readOnly ? 0 : S_IWUSR));
-
-    if (_file < 0) {
-        error(GERIUM_RESULT_ERROR_UNKNOWN); // TODO: add err
-    }
-}
-
-MacOSFile::~MacOSFile() {
-    if (_data) {
-        ::munmap(_data, onGetSize());
-    }
-
-    if (_file >= 0) {
-        ::close(_file);
-    }
+MacOSFile::MacOSFile(gerium_utf8_t path, bool readOnly) : unix::UnixFile(path, readOnly) {
 }
 
 std::string MacOSFile::getPath(NSSearchPathDirectory directory) noexcept {
@@ -87,55 +44,6 @@ NSString* MacOSFile::appendBundleId(NSString* path) {
         dir = [path stringByAppendingPathComponent:[[NSProcessInfo processInfo] processName]];
     }
     return dir;
-}
-
-gerium_uint64_t MacOSFile::onGetSize() noexcept {
-    struct stat stat {};
-
-    ::fstat(_file, &stat);
-    return (gerium_uint64_t) stat.st_size;
-}
-
-void MacOSFile::onSeek(gerium_uint64_t offset, gerium_file_seek_t seek) noexcept {
-    int whence;
-    switch (seek) {
-        case GERIUM_FILE_SEEK_BEGIN:
-            whence = SEEK_SET;
-            break;
-        case GERIUM_FILE_SEEK_CURRENT:
-            whence = SEEK_CUR;
-            break;
-        case GERIUM_FILE_SEEK_END:
-            whence = SEEK_END;
-            break;
-        default:
-            assert(!"unreachable code");
-            whence = SEEK_SET;
-            break;
-    }
-    ::lseek(_file, off_t(offset), seek);
-}
-
-void MacOSFile::onWrite(gerium_cdata_t data, gerium_uint32_t size) {
-    if (::write(_file, (const void*) data, (size_t) size) < 0) {
-        error(GERIUM_RESULT_ERROR_UNKNOWN); // TODO: add err
-    }
-}
-
-gerium_uint32_t MacOSFile::onRead(gerium_data_t data, gerium_uint32_t size) noexcept {
-    return (gerium_uint32_t)::read(_file, (void*) data, (size_t) size);
-}
-
-gerium_data_t MacOSFile::onMap() noexcept {
-    if (!_data) {
-        _data = ::mmap(nullptr,
-                       onGetSize(),
-                       PROT_READ | (isReadOnly() ? 0 : PROT_WRITE),
-                       isReadOnly() ? MAP_PRIVATE : MAP_SHARED,
-                       _file,
-                       0);
-    }
-    return _data;
 }
 
 } // namespace macos
