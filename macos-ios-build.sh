9ba36f546c3254c2bfa249b2455fdb34a9e29cee
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-12-08 21:29:07 +0300

    Update ios build script

--- macos-ios-build.sh
+++ macos-ios-build.sh
@@ -1,72 +1,72 @@
 #!/bin/bash
 
-CONFIG=${1:-Debug}
-BUILD_DIR=${2:-xcbuild}
+set -e
 
-# rm -rf $BUILD_DIR
-mkdir -p \
-    $BUILD_DIR/ios-arm64\
-    $BUILD_DIR/ios-x86_64\
-    $BUILD_DIR/ios-simulator\
-    $BUILD_DIR/macos-arm64\
-    $BUILD_DIR/macos-x86_64
+CONFIG=Debug
+BUILD_ROOT_DIR=xcbuild
 
-cmake -S . -B $BUILD_DIR/ios-arm64 \
-    -DCMAKE_OSX_ARCHITECTURES=arm64 \
-    -DCMAKE_SYSTEM_NAME=iOS \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
-    -DCMAKE_INSTALL_PREFIX=install/ios-arm64 \
-    -DGERIUM_BUILD_SAMPLE=OFF
-cmake --build $BUILD_DIR/ios-arm64 --config $CONFIG
-cmake --install $BUILD_DIR/ios-arm64 --config $CONFIG
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --config)
+      CONFIG="$2"
+      shift
+      shift
+      ;;
+    --build-dir)
+      BUILD_ROOT_DIR="$2"
+      shift
+      shift
+      ;;
+    *)
+      echo "Unknown arg $1"
+      exit 1
+      ;;
+  esac
+done
 
-cmake -S . -B $BUILD_DIR/ios-x86_64 \
-    -DCMAKE_OSX_ARCHITECTURES=x86_64 \
-    -DCMAKE_SYSTEM_NAME=iOS \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
-    -DCMAKE_INSTALL_PREFIX=install/ios-x86_64 \
-    -DGERIUM_BUILD_SAMPLE=OFF
-cmake --build $BUILD_DIR/ios-x86_64 --config $CONFIG
-cmake --install $BUILD_DIR/ios-x86_64 --config $CONFIG
+TARGETS=(
+  "ios-arm64 arm64 iOS 13.0"
+  "ios-simulator-x86_64 x86_64 iOS 13.0"
+  "ios-simulator-arm64 arm64 iOS 13.0"
+  "macos-arm64 arm64 Darwin 13.0"
+  "macos-x86_64 x86_64 Darwin 13.0"
+)
 
-cmake -S . -B $BUILD_DIR/ios-simulator \
-    -DCMAKE_OSX_ARCHITECTURES=arm64 \
-    -DCMAKE_SYSTEM_NAME=iOS \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
-    -DCMAKE_INSTALL_PREFIX=install/ios-simulator \
-    -DGERIUM_BUILD_SAMPLE=OFF
-cmake --build $BUILD_DIR/ios-simulator --config $CONFIG
-cmake --install $BUILD_DIR/ios-simulator --config $CONFIG
+build_target() {
+  local BUILD_DIR="$BUILD_ROOT_DIR/$1"
+  local ARCH="$2"
+  local SYSTEM_NAME="$3"
+  local DEPLOYMENT_TARGET="$4"
+  local INSTALL_DIR="${BUILD_DIR}/install"
 
-cmake -S . -B $BUILD_DIR/macos-arm64 \
-    -DCMAKE_OSX_ARCHITECTURES=arm64 \
-    -DCMAKE_SYSTEM_NAME=Darwin \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
-    -DCMAKE_INSTALL_PREFIX=install/macos-arm64 \
-    -DGERIUM_BUILD_SAMPLE=OFF
-cmake --build $BUILD_DIR/macos-arm64 --config $CONFIG
-cmake --install $BUILD_DIR/macos-arm64 --config $CONFIG
+  rm -rf $BUILD_DIR
+  mkdir -p $BUILD_DIR
 
-cmake -S . -B $BUILD_DIR/macos-x86_64 \
-    -DCMAKE_OSX_ARCHITECTURES=x86_64 \
-    -DCMAKE_SYSTEM_NAME=Darwin \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
-    -DCMAKE_INSTALL_PREFIX=install/macos-x86_64 \
-    -DGERIUM_BUILD_SAMPLE=OFF
-cmake --build $BUILD_DIR/macos-x86_64 --config $CONFIG
-cmake --install $BUILD_DIR/macos-x86_64 --config $CONFIG
+  cmake -S . -B $BUILD_DIR \
+      -DCMAKE_OSX_ARCHITECTURES=$ARCH \
+      -DCMAKE_SYSTEM_NAME=$SYSTEM_NAME \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=$DEPLOYMENT_TARGET \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+
+  cmake --build $BUILD_DIR --config $CONFIG
+  cmake --install $BUILD_DIR --config $CONFIG --verbose
+}
+
+for target in "${TARGETS[@]}"; do
+  build_target $target
+done
 
 xcodebuild -create-xcframework \
-    -library install/ios-arm64/lib/libyourlibrary.a \
-    -headers install/ios-arm64/include \
-    -library install/ios-x86_64/lib/libyourlibrary.a \
-    -headers install/ios-x86_64/include \
-    -library install/ios-simulator/lib/libyourlibrary.a \
-    -headers install/ios-simulator/include \
-    -library install/macos-arm64/lib/libyourlibrary.a \
-    -headers install/macos-arm64/include \
-    -library install/macos-x86_64/lib/libyourlibrary.a \
-    -headers install/macos-x86_64/include \
-    -output build/yourlibrary.xcframework
+    -library build/ios-arm64/install/lib/gerium.a \
+    -headers build/ios-arm64/install/include \
+    -library build/ios-simulator-x86_64/install/lib/gerium.a \
+    -headers build/ios-simulator-x86_64/install/include \
+    -library build/ios-simulator-arm64/install/lib/gerium.a \
+    -headers build/ios-simulator-arm64/install/include \
+    -library build/macos-arm64/install/lib/gerium.a \
+    -headers build/macos-arm64/install/include \
+    -library build/macos-x86_64/install/lib/gerium.a \
+    -headers build/macos-x86_64/install/include \
+    -output universal/gerium.xcframework
 
-# rm -rf $BUILD_DIR
+rm -rf $BUILD_ROOT_DIR
