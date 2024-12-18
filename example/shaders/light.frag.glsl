ea829d04b10c6b29e67caccaad0ef7c72c7aced2
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-12-18 14:38:57 +0300

    add tricubic filter

--- example/shaders/light.frag.glsl
+++ example/shaders/light.frag.glsl
@@ -42,10 +42,52 @@ layout(location = 1) out vec4 outDiffuse;
 //     a = scatteringTransmittance.a;
 // }
 
+// https://gist.github.com/Fewes/59d2c831672040452aa77da6eaab2234
+vec4 textureTricubic(vec3 coord) {
+    vec3 tsize = vec3(textureSize(texIntegratedLightScattering, 0));
+	vec3 coordGrid = coord * tsize - 0.5;
+	vec3 index = floor(coordGrid);
+	vec3 fraction = coordGrid - index;
+	vec3 oneFrac = 1.0 - fraction;
+
+	vec3 w0 = 1.0 / 6.0 * oneFrac * oneFrac * oneFrac;
+	vec3 w1 = 2.0 / 3.0 - 0.5 * fraction * fraction * (2.0 - fraction);
+	vec3 w2 = 2.0 / 3.0 - 0.5 * oneFrac * oneFrac * (2.0 - oneFrac);
+	vec3 w3 = 1.0 / 6.0 * fraction * fraction * fraction;
+
+	vec3 g0 = w0 + w1;
+	vec3 g1 = w2 + w3;
+	vec3 mult = 1.0 / tsize;
+	vec3 h0 = mult * ((w1 / g0) - 0.5 + index); //h0 = w1/g0 - 1, move from [-0.5, tsize-0.5] to [0,1]
+	vec3 h1 = mult * ((w3 / g1) + 1.5 + index); //h1 = w3/g1 + 1, move from [-0.5, tsize-0.5] to [0,1]
+
+	// Fetch the eight linear interpolations
+	// Weighting and fetching is interleaved for performance and stability reasons
+	vec4 tex000 = texture(texIntegratedLightScattering, h0, 0.0);
+	vec4 tex100 = texture(texIntegratedLightScattering, vec3(h1.x, h0.y, h0.z), 0.0f);
+	tex000 = mix(tex100, tex000, g0.x); // Weigh along the x-direction
+
+	vec4 tex010 = texture(texIntegratedLightScattering, vec3(h0.x, h1.y, h0.z), 0.0f);
+	vec4 tex110 = texture(texIntegratedLightScattering, vec3(h1.x, h1.y, h0.z), 0.0f);
+	tex010 = mix(tex110, tex010, g0.x); // Weigh along the x-direction
+	tex000 = mix(tex010, tex000, g0.y); // Weigh along the y-direction
+
+	vec4 tex001 = texture(texIntegratedLightScattering, vec3(h0.x, h0.y, h1.z), 0.0f);
+	vec4 tex101 = texture(texIntegratedLightScattering, vec3(h1.x, h0.y, h1.z), 0.0f);
+	tex001 = mix(tex101, tex001, g0.x); // Weigh along the x-direction
+
+	vec4 tex011 = texture(texIntegratedLightScattering, vec3(h0.x, h1.y, h1.z), 0.0f);
+	vec4 tex111 = texture(texIntegratedLightScattering, vec3(h1), 0.0f);
+	tex011 = mix(tex111, tex011, g0.x); // Weigh along the x-direction
+	tex001 = mix(tex011, tex001, g0.y); // Weigh along the y-direction
+
+	return mix(tex001, tex000, g0.z); // Weigh along the z-direction
+}
+
 void calcVolumetricFog(vec3 worldPos, out vec3 color, out float a) {
     vec3 froxelUvw = worldToUv(worldPos, fog.biasNearFarPow.y, fog.biasNearFarPow.z, fog.viewProj);
 
-    vec4  scatteredLight = textureLod(texIntegratedLightScattering, froxelUvw, 0);
+    vec4  scatteredLight = textureTricubic(froxelUvw);
     float transmittance  = scatteredLight.a;
 
     a = transmittance;
